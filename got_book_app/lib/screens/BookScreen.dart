4eb9e127826f3723769db25a7f97979423a5907f
Carolyn Barbour <carolyn@boundary.co.uk>
2022-12-22 10:37:16 +0000

    * Playing around with nicer layout

--- got_book_app/lib/screens/BookScreen.dart
+++ got_book_app/lib/screens/BookScreen.dart
@@ -40,23 +40,74 @@ class _BookScreenState extends State<BookScreen> {
             backgroundColor: Colors.black,
             foregroundColor: Colors.white),
         body: SafeArea(
-            child: Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Number of pages: ${book.numberOfPages}",
-                        style: const TextStyle(color: Colors.grey)),
-                    Text("Country: ${book.country}",
-                        style: const TextStyle(color: Colors.grey)),
-                    Text(
-                        "Date of Release: ${DateFormat('yyyy-MM').format(dateTimeOfRelease)}",
-                        style: const TextStyle(color: Colors.grey)),
-                    Text(
-                        "Author(s): ${book.authors.toString().replaceAll('[', '').replaceAll(']', '')}",
-                        style: const TextStyle(color: Colors.grey))
-                  ],
-                ))));
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("Number of pages:",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("Country:",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("Date of Release:",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("Author(s):",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 16.0)))
+                        ],
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("${book.numberOfPages}",
+                                  style: const TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text("${book.country}",
+                                  style: const TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text(
+                                  DateFormat('MM-yyyy')
+                                      .format(dateTimeOfRelease),
+                                  style: const TextStyle(
+                                      color: Colors.grey, fontSize: 16.0))),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Text(
+                                  book.authors
+                                      .toString()
+                                      .replaceAll('[', '')
+                                      .replaceAll(']', ''),
+                                  style: const TextStyle(
+                                      color: Colors.grey, fontSize: 16.0)))
+                        ],
+                      )
+                    ]))));
   }
 }
